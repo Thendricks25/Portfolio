b45e4409642e28809cdbeed46db1ea1fc316709e
Tateyana Hendricks <ginaskco25@gmail.com>
2020-08-27 07:42:51 -0700

    fixed the contact form to be resonably sized on desktop and mobile, fixed the title to be in a row on desktop and a column on mobile.

--- lib/main.dart
+++ lib/main.dart
@@ -73,15 +73,29 @@ class _MyHomePageState extends State<MyHomePage> {
                   Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 10, vertical: 50),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TitleLine(text: 'iAm'),
-                        TitleLine(
-                            text: 'TateyanaHendricks',
-                            color: Colors.lightBlueAccent),
-                        TitleLine(text: ' = flutterDeveloper( );'),
-                      ],
+                    child: getValueForScreenType(
+                      context: context,
+                      desktop: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TitleLine(text: 'iAm'),
+                          TitleLine(
+                              text: 'TateyanaHendricks',
+                              color: Colors.lightBlueAccent),
+                          TitleLine(text: ' = flutterDeveloper( );'),
+                        ],
+                      ),
+                      mobile: Center(
+                        child: Column(
+                          children: [
+                            TitleLine(text: 'iAm'),
+                            TitleLine(
+                                text: 'TateyanaHendricks',
+                                color: Colors.lightBlueAccent),
+                            TitleLine(text: ' = flutterDeveloper( );'),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                   getValueForScreenType<Widget>(
@@ -282,86 +296,91 @@ class ContactFormFields extends StatefulWidget {
 class _ContactFormFieldsState extends State<ContactFormFields> {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: checkTheForm,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 150.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: controller,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return "This field cannot be empty";
-                }
-                return null;
-              },
-              onSaved: (value) {
-                name = value;
-              },
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Name',
-                  hintStyle: TextStyle(color: Colors.black54),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  contentPadding: EdgeInsets.all(8)),
-              textCapitalization: TextCapitalization.words,
-              style: TextStyle(color: Colors.black),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            TextFormField(
-              controller: controller2,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return "This field cannot be empty";
-                }
-                return null;
-              },
-              onSaved: (value) {
-                email = value;
-              },
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Email',
-                  hintStyle: TextStyle(color: Colors.black54),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  contentPadding: EdgeInsets.all(8)),
-              textCapitalization: TextCapitalization.words,
-              style: TextStyle(color: Colors.black),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            TextFormField(
-              controller: controller3,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return "This field cannot be empty";
-                }
-                return null;
-              },
-              onSaved: (value) {
-                message = value;
-              },
-              maxLines: 15,
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Message',
-                  hintStyle: TextStyle(color: Colors.black54),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  contentPadding: EdgeInsets.all(8)),
-              textCapitalization: TextCapitalization.words,
-              style: TextStyle(color: Colors.black),
-            ),
-          ],
+    return Container(
+      width: 350,
+      child: Form(
+        key: checkTheForm,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: getValueForScreenType(
+                  context: context, mobile: 50, desktop: 0, tablet: 0)),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: controller,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "This field cannot be empty";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  name = value;
+                },
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Name',
+                    hintStyle: TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    contentPadding: EdgeInsets.all(8)),
+                textCapitalization: TextCapitalization.words,
+                style: TextStyle(color: Colors.black),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                controller: controller2,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "This field cannot be empty";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Email',
+                    hintStyle: TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    contentPadding: EdgeInsets.all(8)),
+                textCapitalization: TextCapitalization.words,
+                style: TextStyle(color: Colors.black),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                controller: controller3,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "This field cannot be empty";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  message = value;
+                },
+                maxLines: 15,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Message',
+                    hintStyle: TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    contentPadding: EdgeInsets.all(8)),
+                textCapitalization: TextCapitalization.words,
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
         ),
       ),
     );
