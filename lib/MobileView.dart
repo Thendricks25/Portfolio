d67a478dbfd00dbf46e22f5423c23c1f8a83678b
Tateyana Hendricks <ginaskco25@gmail.com>
2020-09-03 07:39:07 -0700

    added app icons, edited the mobile project tiles.

--- lib/MobileView.dart
+++ lib/MobileView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import 'ContactForm.dart';
 import 'ProjectTiles.dart';
@@ -63,71 +64,92 @@ print( "A software developer that uses the'''
                     Text(
                       'Unless otherwise specified in the app description I am the sole'
                       'developer on these projects.',
-                      style: TextStyle(fontSize: 18),
+                      style: TextStyle(
+                          fontSize: getValueForScreenType(
+                              context: context, desktop: 18, mobile: 14)),
                       textAlign: TextAlign.center,
                     ),
                     Container(
-                      height: 900,
+                      height: 500,
                       child:
                           ListView(scrollDirection: Axis.horizontal, children: [
-                        Card(
-                          child: ProjectTile(
-                            appTitle: 'Weather',
-                            appDescription:
-                                'A mobile application that uses the devices GPS to find the users'
-                                ' location and displays the current and forecasted weather '
-                                'information for their current location. The weather data is '
-                                'obtained from several weather APIs. The User can also search for '
-                                'weather for another location.',
-                            images: [
-                              ExactAssetImage('images/weather main.png'),
-                              ExactAssetImage('images/weather forcaste.png'),
-                              ExactAssetImage('images/weather search.png'),
-                            ],
-                            github: false,
-                            nolan: false,
-                          ),
+                        MobileProjectTiles(
+                          title: 'Weather',
+                          appIcon: 'images/weather app icon.png',
+                          shortDescription:
+                              'A mobile weather application that...',
+                          longDescription:
+                              'A mobile weather application that uses the devices GPS to find the users'
+                              ' location and displays the current and forecasted weather '
+                              'information for their current location. The weather data is '
+                              'obtained from several weather APIs. The User can also search for '
+                              'weather for another location.',
+                          images: [
+                            ExactAssetImage('images/weather main.png'),
+                            ExactAssetImage('images/weather forcaste.png'),
+                            ExactAssetImage('images/weather search.png'),
+                          ],
+                          nolan: false,
+                          github: false,
                         ),
-                        Card(
-                          child: ProjectTile(
-                            appTitle: 'Shopping List',
-                            appDescription:
-                                'A mobile and web application that uses firebase authentication '
-                                'to allow users to register for and sign in to their account. '
-                                'Users can enter new items into their shopping list and double '
-                                'tap the items to delete them from the list. The app uses '
-                                'Firestore cloud database for storage.  The app syncs across '
-                                'platforms so the user can manage their shopping list from '
-                                'their smartphone or browser.',
-                            images: [
-                              ExactAssetImage('images/Shoppinglist log in.png'),
-                              ExactAssetImage('images/Shopping list main.png'),
-                              ExactAssetImage(
-                                  'images/Shopping list change bkgrd.png'),
-                              ExactAssetImage(
-                                  'images/Shopping list second bkgd.png'),
-                            ],
-                            github: false,
-                            nolan: false,
-                          ),
+                        MobileProjectTiles(
+                          title: 'Shopping List',
+                          appIcon: 'images/shopping list icon2.png',
+                          shortDescription:
+                              'A shopping list that can be managed from a smartphone or browser...',
+                          longDescription:
+                              'A mobile and web application that uses firebase authentication '
+                              'to allow users to register for and sign in to their account. '
+                              'Users can enter new items into their shopping list and double '
+                              'tap the items to delete them from the list. The app uses '
+                              'Firestore cloud database for storage.  The app syncs across '
+                              'platforms so the user can manage their shopping list from '
+                              'their smartphone or browser.',
+                          images: [
+                            ExactAssetImage('images/Shoppinglist log in.png'),
+                            ExactAssetImage('images/Shopping list main.png'),
+                            ExactAssetImage(
+                                'images/Shopping list change bkgrd.png'),
+                            ExactAssetImage(
+                                'images/Shopping list second bkgd.png'),
+                          ],
+                          github: false,
+                          nolan: false,
                         ),
-                        Card(
-                          child: ProjectTile(
-                            appTitle: 'Breathe',
-                            appDescription: 'A mobile application '
-                                'that guides the user through a breathing mediation exercise. '
-                                'This app was created for the 2020 Flutter Hackathon in '
-                                'collaboration with myself and Nolan Sherman',
-                            images: [
-                              ExactAssetImage('images/home.png'),
-                              ExactAssetImage('images/Breathe_app_demo.gif'),
-                              ExactAssetImage('images/stats.png'),
-                            ],
-                            url: "https://github.com/weare10/breathe",
-                            nolan: true,
-                            github: true,
-                          ),
-                        ),
+                        MobileProjectTiles(
+                          title: 'Breathe',
+                          appIcon: 'images/Breathe app icon.png',
+                          nolan: true,
+                          shortDescription: 'A Breathing Mediation app...',
+                          longDescription: 'A mobile application '
+                              'that guides the user through a breathing mediation exercise. '
+                              'This app was created for the 2020 Flutter Hackathon in '
+                              'collaboration with myself and Nolan Sherman',
+                          images: [
+                            ExactAssetImage('images/home.png'),
+                            ExactAssetImage('images/Breathe_app_demo.gif'),
+                            ExactAssetImage('images/stats.png'),
+                          ],
+                          github: true,
+                          url: "https://github.com/weare10/breathe",
+                        )
+//                        Card(
+//                          child: ProjectTile(
+//                            appTitle: 'Breathe',
+//                            appDescription: 'A mobile application '
+//                                'that guides the user through a breathing mediation exercise. '
+//                                'This app was created for the 2020 Flutter Hackathon in '
+//                                'collaboration with myself and Nolan Sherman',
+//                            images: [
+//                              ExactAssetImage('images/home.png'),
+//                              ExactAssetImage('images/Breathe_app_demo.gif'),
+//                              ExactAssetImage('images/stats.png'),
+//                            ],
+//                            url: "https://github.com/weare10/breathe",
+//                            nolan: true,
+//                            github: true,
+//                          ),
+//                        ),
                       ]),
                     ),
                   ]),
--- lib/ProjectTiles.dart
+++ lib/ProjectTiles.dart
@@ -34,73 +34,239 @@ class ProjectTile extends StatelessWidget {
         horizontal:
             getValueForScreenType(context: context, mobile: 50, desktop: 100),
       ),
-      child: Container(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                appTitle,
-                style: TextStyle(fontSize: 30),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //if mobile display in column, if desktop display in row
-              //getValueForScreenType<Widget>(),
-              Row(
-                children: [
-                  Container(
-                    height: 500,
-                    width: 300,
-                    child: Carousel(
-                      images: images,
-                      dotColor: Colors.lightBlueAccent,
-                      dotIncreasedColor: Colors.lightBlueAccent,
-                      autoplay: false,
+      child: Center(
+        child: Container(
+          child: Row(children: [
+            Column(
+              children: [
+                Text(
+                  appTitle,
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                //if mobile display in column, if desktop display in row
+                //getValueForScreenType<Widget>(),
+                Row(
+                  children: [
+                    Container(
+                      height: getValueForScreenType(
+                          context: context, desktop: 500, mobile: 250),
+                      width: getValueForScreenType(
+                          context: context, desktop: 300, mobile: 150),
+                      child: Carousel(
+                        images: images,
+                        dotColor: Colors.lightBlueAccent,
+                        dotIncreasedColor: Colors.lightBlueAccent,
+                        autoplay: false,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  nolan
-                      ? Column(
-                          children: [
-                            Text(
-                              'Developers:',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            Text(
-                              'Tateyana Hendricks',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            InkWell(
-                              child: Text(
-                                'Nolan Sherman',
-                                style: TextStyle(
-                                    color: Colors.lightBlueAccent,
-                                    fontSize: 20,
-                                    decoration: TextDecoration.underline),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    nolan
+                        ? Column(
+                            children: [
+                              Text(
+                                'Developers:',
+                                style: TextStyle(fontSize: 20),
                               ),
+                              Text(
+                                'Tateyana Hendricks',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              InkWell(
+                                child: Text(
+                                  'Nolan Sherman',
+                                  style: TextStyle(
+                                      color: Colors.lightBlueAccent,
+                                      fontSize: 20,
+                                      decoration: TextDecoration.underline),
+                                ),
+                                onTap: () {
+                                  launchURL(
+                                      'https://www.linkedin.com/in/nolanrsherman');
+                                },
+                                hoverColor: Colors.white,
+                              ),
+                            ],
+                          )
+                        : Container(),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 300,
+                  child: Text(
+                    appDescription,
+                    style: TextStyle(
+                        fontSize: getValueForScreenType(
+                            context: context, desktop: 25, mobile: 15)),
+                  ),
+                ),
+                //github icon
+                github
+                    ? FlatButton(
+                        child:
+                            Image(image: AssetImage('images/github icon.png')),
+                        splashColor: Colors.white,
+                        onPressed: () {
+                          launchURL(url);
+                        },
+                        focusColor: Colors.white,
+                      )
+                    : Container(),
+              ],
+            ),
+            SizedBox(height: 100),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileProjectTiles extends StatelessWidget {
+  MobileProjectTiles(
+      {this.title,
+      this.appIcon,
+      this.shortDescription,
+      this.longDescription,
+      this.images,
+      this.nolan,
+      this.github,
+      this.url});
+  final String title;
+  final String appIcon;
+  final String shortDescription;
+  final String longDescription;
+  final images;
+  final bool nolan;
+  final bool github;
+  final url;
+
+  launchURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 500,
+      child: Card(
+        margin: EdgeInsets.all(28),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Container(
+                    width: 150,
+                    height: 150,
+                    child: Image(image: AssetImage(appIcon))),
+              ),
+              nolan
+                  ? Column(
+                      children: [
+                        Text(
+                          'Developers:',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          'Tateyana Hendricks',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        InkWell(
+                          child: Text(
+                            'Nolan Sherman',
+                            style: TextStyle(
+                                color: Colors.lightBlueAccent,
+                                fontSize: 20,
+                                decoration: TextDecoration.underline),
+                          ),
+                          onTap: () {
+                            launchURL(
+                                'https://www.linkedin.com/in/nolanrsherman');
+                          },
+                          hoverColor: Colors.white,
+                        ),
+                      ],
+                    )
+                  : Container(),
+              Text(shortDescription),
+              InkWell(
+                child: Text(
+                  'Learn More',
+                  style: TextStyle(
+                      color: Colors.lightBlueAccent,
+                      decoration: TextDecoration.underline),
+                ),
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('App Description'),
+                          content: Text(longDescription),
+                          actions: [
+                            InkWell(
+                              child: Text('Close'),
                               onTap: () {
-                                launchURL(
-                                    'https://www.linkedin.com/in/nolanrsherman');
+                                Navigator.pop(context);
                               },
-                              hoverColor: Colors.white,
                             ),
                           ],
-                        )
-                      : Container(),
-                ],
+                        );
+                      });
+                },
               ),
-              SizedBox(height: 20),
-              Text(
-                appDescription,
-                style: TextStyle(fontSize: 25),
-                overflow: TextOverflow.clip,
-              ),
-              //github icon
+              OutlineButton(
+                  child: Text('View App Screenshots'),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Screenshots'),
+                          content: Container(
+                            height: 300,
+                            width: 150,
+                            child: Carousel(
+                              images: images,
+                              dotColor: Colors.lightBlueAccent,
+                              dotIncreasedColor: Colors.lightBlueAccent,
+                              autoplay: false,
+                            ),
+                          ),
+                          actions: [
+                            FlatButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Close'))
+                          ],
+                        );
+                      },
+                    );
+                  }),
               github
                   ? FlatButton(
                       child: Image(image: AssetImage('images/github icon.png')),
@@ -111,8 +277,9 @@ class ProjectTile extends StatelessWidget {
                       focusColor: Colors.white,
                     )
                   : Container(),
-              SizedBox(height: 100),
-            ]),
+            ],
+          ),
+        ),
       ),
     );
   }
